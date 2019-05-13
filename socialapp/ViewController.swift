4fcb2d6743020924b7b039632517ce318d12a303
cowpro <kian.chakamian@gmail.com>
2019-05-12 18:48:41 -0700

    Initial Commit

---
+++ socialapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  socialapp
+//
+//  Created by Kian Chakamian on 5/12/19.
+//  Copyright © 2019 Kian Chakamian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
